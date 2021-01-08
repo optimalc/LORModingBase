e09c359cb98998586830c09121e8e829f6edce2e
Sungjin Sin <sinsung6722@gmail.com>
2021-01-08 13:19:19 +0900

    Check Data class abstracted

--- LORModingBase/LORModingBase/DM/CheckDatas.cs
+++ LORModingBase/LORModingBase/DM/CheckDatas.cs
@@ -22,68 +22,10 @@ namespace LORModingBase.DM
         public static string CheckAllDatas()
         {
             LOG_DATA = "";
-            CheckDatas_Critical();
-            CheckDatas_Caution();
+            CriticalCheck.CriticalCheckAll();
+            CautionCheck.CautionCheckAll();
             return LOG_DATA;
         }
-
-        /// <summary>
-        /// Check critical infos
-        /// </summary>
-        public static void CheckDatas_Critical()
-        {
-            #region Key Page Check
-            string CRITICAL = DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.LOGGING, $"Critical");
-            List<string> KEY_PAGE_ID_LIST = new List<string>();
-            DM.EditGameData_BookInfos.StaticEquipPage.rootDataNode.ActionXmlDataNodesByPath("Book", (XmlDataNode bookNode) =>
-            {
-                string BOOK_NODE_ID = bookNode.GetAttributesSafe("ID");
-                string BOOK_NODE_NAME = bookNode.GetInnerTextByPath("Name");
-
-                if (string.IsNullOrEmpty(BOOK_NODE_ID))
-                    MakeCriticalMessage("KeyPage_Critical_1", BOOK_NODE_NAME);
-                if (string.IsNullOrEmpty(BOOK_NODE_NAME))
-                    MakeCriticalMessage("KeyPage_Critical_2", BOOK_NODE_ID);
-
-                if(!string.IsNullOrEmpty(BOOK_NODE_ID))
-                {
-                    if(KEY_PAGE_ID_LIST.Contains(BOOK_NODE_ID))
-                        MakeCriticalMessage("KeyPage_Critical_3", BOOK_NODE_ID);
-                    KEY_PAGE_ID_LIST.Add(BOOK_NODE_ID);
-                }
-            });
-            #endregion
-
-            #region Cards Check
-            List<string> CARD_PAGE_ID_LIST = new List<string>();
-            DM.EditGameData_CardInfos.StaticCard.rootDataNode.ActionXmlDataNodesByPath("Card", (XmlDataNode bookNode) =>
-            {
-                string CARD_NODE_ID = bookNode.GetAttributesSafe("ID");
-                string CARD_NODE_NAME = bookNode.GetInnerTextByPath("Name");
-
-                if (string.IsNullOrEmpty(CARD_NODE_ID))
-                    MakeCriticalMessage("Card_Critical_1", CARD_NODE_NAME);
-                if (string.IsNullOrEmpty(CARD_NODE_NAME))
-                    MakeCriticalMessage("Card_Critical_2", CARD_NODE_ID);
-
-                if (!string.IsNullOrEmpty(CARD_NODE_ID))
-                {
-                    if (CARD_PAGE_ID_LIST.Contains(CARD_NODE_ID))
-                        MakeCriticalMessage("Card_Critical_3", CARD_NODE_ID);
-                    CARD_PAGE_ID_LIST.Add(CARD_NODE_ID);
-                }
-            });
-            #endregion
-        }
-
-        /// <summary>
-        /// Check caution infos
-        /// </summary>
-        public static void CheckDatas_Caution()
-        {
-
-        }
-    
 
         /// <summary>
         /// Make ciritical log message
@@ -105,6 +47,68 @@ namespace LORModingBase.DM
         {
             string CAUTION = DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.LOGGING, $"Caution");
             LOG_DATA += $"[-][{CAUTION}] {String.Format(DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.LOGGING, languageID), args)}\n";
+        }
+    }
+
+    class CriticalCheck
+    {
+        public static void CriticalCheckAll()
+        {
+            KeyPageCheck();
+            CardCheck();
+        }
+
+        public static void KeyPageCheck()
+        {
+            string CRITICAL = DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.LOGGING, $"Critical");
+            List<string> KEY_PAGE_ID_LIST = new List<string>();
+            DM.EditGameData_BookInfos.StaticEquipPage.rootDataNode.ActionXmlDataNodesByPath("Book", (XmlDataNode bookNode) =>
+            {
+                string BOOK_NODE_ID = bookNode.GetAttributesSafe("ID");
+                string BOOK_NODE_NAME = bookNode.GetInnerTextByPath("Name");
+
+                if (string.IsNullOrEmpty(BOOK_NODE_ID))
+                    CheckDatas.MakeCriticalMessage("KeyPage_Critical_1", BOOK_NODE_NAME);
+                if (string.IsNullOrEmpty(BOOK_NODE_NAME))
+                    CheckDatas.MakeCriticalMessage("KeyPage_Critical_2", BOOK_NODE_ID);
+
+                if (!string.IsNullOrEmpty(BOOK_NODE_ID))
+                {
+                    if (KEY_PAGE_ID_LIST.Contains(BOOK_NODE_ID))
+                        CheckDatas.MakeCriticalMessage("KeyPage_Critical_3", BOOK_NODE_ID);
+                    KEY_PAGE_ID_LIST.Add(BOOK_NODE_ID);
+                }
+            });
+        }
+
+        public static void CardCheck()
+        {
+            List<string> CARD_PAGE_ID_LIST = new List<string>();
+            DM.EditGameData_CardInfos.StaticCard.rootDataNode.ActionXmlDataNodesByPath("Card", (XmlDataNode bookNode) =>
+            {
+                string CARD_NODE_ID = bookNode.GetAttributesSafe("ID");
+                string CARD_NODE_NAME = bookNode.GetInnerTextByPath("Name");
+
+                if (string.IsNullOrEmpty(CARD_NODE_ID))
+                    CheckDatas.MakeCriticalMessage("Card_Critical_1", CARD_NODE_NAME);
+                if (string.IsNullOrEmpty(CARD_NODE_NAME))
+                    CheckDatas.MakeCriticalMessage("Card_Critical_2", CARD_NODE_ID);
+
+                if (!string.IsNullOrEmpty(CARD_NODE_ID))
+                {
+                    if (CARD_PAGE_ID_LIST.Contains(CARD_NODE_ID))
+                        CheckDatas.MakeCriticalMessage("Card_Critical_3", CARD_NODE_ID);
+                    CARD_PAGE_ID_LIST.Add(CARD_NODE_ID);
+                }
+            });
+        }
+    }
+
+    class CautionCheck
+    {
+        public static void CautionCheckAll()
+        {
+
         }
     }
 }
