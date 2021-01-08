e1851a7dabb38f926ee9ebfbde6daeb91e9dd76d
Sungjin Sin <sinsung6722@gmail.com>
2021-01-08 13:59:37 +0900

    Key page passive bug fixed

--- LORModingBase/LORModingBase/SubWindows/Global_InputInfoWithSearchWindow.xaml.cs
+++ LORModingBase/LORModingBase/SubWindows/Global_InputInfoWithSearchWindow.xaml.cs
@@ -93,7 +93,7 @@ namespace LORModingBase.SubWindows
                         string PASSIVE_ID = passiveNode.GetAttributesSafe("ID");
                         string PASSIVE_DES = DM.LocalizedGameDescriptions.GetDescriptionForPassive(passiveNode.GetAttributesSafe("ID"));
 
-                        if (!DS.FilterDatas.EXCLUDE_PASSIVE_CODE.Contains(PASSIVE_ID))
+                        if (!DS.FilterDatas.EXCLUDE_PASSIVE_CODE.Contains(PASSIVE_ID) && Convert.ToInt32(PASSIVE_ID) > 10000)
                             selectItems.Add($"{PASSIVE_DES}:{PASSIVE_ID}");
                     });
                     searchTypes.AddRange(DM.GetLocalizedFilterList.GetLocalizedPassives());
