78456fa90d681a43a490c458b1cd7432c5a01318
Sungjin Sin <sinsung6722@gmail.com>
2021-01-12 19:12:38 +0900

    Check logic is updated

--- LORModingBase/LORModingBase/DM/CheckDatas.cs
+++ LORModingBase/LORModingBase/DM/CheckDatas.cs
@@ -28,43 +28,68 @@ namespace LORModingBase.DM
 
         public static void CheckCriticalMessageWithSyntaxString()
         {
+            #region Critical Page check logic
             CheckDatasBySyntaxCheckStrings(
-               DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnCriticalPage%").Split('$')[0],
-               "Book$ID", DM.EditGameData_BookInfos.StaticEquipPage.rootDataNode,
-               new List<string>() { "Book$ID#BDI", "Book/Name#B" ,
+              DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnCriticalPage%").Split('$')[0],
+              "Book$ID", DM.EditGameData_BookInfos.StaticEquipPage.rootDataNode,
+                new List<string>() { "Book$ID#BDI", "Book/Name#B" ,
                                     "Book/EquipEffect/HP#BI", "Book/EquipEffect/Break#BI",
                                     "Book/EquipEffect/SpeedMin#BI", "Book/EquipEffect/Speed#BI",
                                     "Book/Chapter#BI", "Book/Episode#BI", "Book/BookIcon#B", "Book/CharacterSkin#B"});
+            #endregion
+            #region Card Page check logic
             CheckDatasBySyntaxCheckStrings(
                 DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnCards%").Split('$')[0],
                 "Card$ID", DM.EditGameData_CardInfos.StaticCard.rootDataNode,
-                new List<string>() { "Card$ID#BD", "Card/Name#B" }); 
+                new List<string>() { "Card$ID#BDI", "Card/Name#B",
+                                     "Card/Artwork#B", "Card/Spec$Cost#BI",
+                                     "Card/BehaviourList/Behaviour$Min#BI", "Card/BehaviourList/Behaviour$Dice#BI"});
+            #endregion
 
+            #region Stage check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnStages%").Split('$')[0],
-             "Stage$id", DM.EditGameData_StageInfo.StaticStageInfo.rootDataNode,
-                new List<string>() { "Stage$id#BD" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnStages%").Split('$')[0],
+                "Stage$id", DM.EditGameData_StageInfo.StaticStageInfo.rootDataNode,
+                new List<string>() { "Stage$id#BDI", "Stage/Name#B",
+                                     "Stage/Wave/Formation#BI", "Stage/Wave/Unit#BI",
+                                     "Stage/FloorNum#BI", "Stage/Chapter#BI"});
+            #endregion
+            #region Enemy check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnEnemy%").Split('$')[0],
-             "Enemy$ID", DM.EditGameData_EnemyInfo.StaticEnemyUnitInfo.rootDataNode,
-                new List<string>() { "Enemy$ID#BD", "Enemy/NameID#B" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnEnemy%").Split('$')[0],
+                "Enemy$ID", DM.EditGameData_EnemyInfo.StaticEnemyUnitInfo.rootDataNode,
+                new List<string>() { "Enemy$ID#BDI", "Enemy/NameID#B",
+                                     "Enemy/MinHeight#BI", "Enemy/MaxHeight#BI",
+                                     "Enemy/BookId#BI", "Enemy/DeckId#BI",
+                                     "Enemy/DropTable/DropItem#BI"});
+            #endregion
+            #region Deck check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnDecks%").Split('$')[0],
-             "Deck$ID", DM.EditGameData_DeckInfo.StaticDeckInfo.rootDataNode,
-                new List<string>() { "Deck$ID#BD" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnDecks%").Split('$')[0],
+                "Deck$ID", DM.EditGameData_DeckInfo.StaticDeckInfo.rootDataNode,
+                new List<string>() { "Deck$ID#BD", "Deck/Card#BI" });
+            #endregion
 
+            #region DropBook check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnDropBook%").Split('$')[0],
-             "BookUse$ID", DM.EditGameData_DropBookInfo.StaticDropBookInfo.rootDataNode,
-                new List<string>() { "BookUse$ID#BD" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnDropBook%").Split('$')[0],
+                "BookUse$ID", DM.EditGameData_DropBookInfo.StaticDropBookInfo.rootDataNode,
+                new List<string>() { "BookUse$ID#BD", "BookUse/TextId#B",
+                                     "BookUse/BookIcon#B", "BookUse/Chapter#BI",
+                                      "BookUse/DropItem#BI"});
+            #endregion
+            #region Passive check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnPassive%").Split('$')[0],
-             "Passive$ID", DM.EditGameData_PassiveInfo.StaticPassiveList.rootDataNode,
-                new List<string>() { "Passive$ID#BD" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnPassive%").Split('$')[0],
+                "Passive$ID", DM.EditGameData_PassiveInfo.StaticPassiveList.rootDataNode,
+                new List<string>() { "Passive$ID#BD", "Passive/Cost#BI" });
+            #endregion
+            #region Card ability check logic
             CheckDatasBySyntaxCheckStrings(
-             DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnCardAbility%").Split('$')[0],
-             "BattleCardAbility$ID", DM.EditGameData_CardAbilityInfo.LocalizedCardAbility.rootDataNode,
-                new List<string>() { "BattleCardAbility$ID#BD" });
+                DM.LocalizeCore.GetLanguageData(DM.LANGUAGE_FILE_NAME.MAIN_WINDOW, $"%BtnCardAbility%").Split('$')[0],
+                "BattleCardAbility$ID", DM.EditGameData_CardAbilityInfo.LocalizedCardAbility.rootDataNode,
+                new List<string>() { "BattleCardAbility$ID#BD" }); 
+            #endregion
         }
 
         /// <summary>
